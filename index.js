e8e999f4670fa49d1382c8bbad704e494f6b8350
alex villy <alexisvilly.work@gmail.com>
2026-03-11 17:22:49 +0100

    CD: Fix npm install conflict with legacy-peer-deps

---
+++ index.js
@@ -0,0 +1,4 @@
+import { registerRootComponent } from 'expo';
+import Homepage from '../ListeTODOUX/screens/Homepage'; // Ou le nom de ton fichier principal (ex: ./HomeScreen)
+
+registerRootComponent(Homepage);
